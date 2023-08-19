b093afd5735673b906b2e2ddfef29424c403907b
augusto.izepon <augustom.izepon@gmail.com>
2023-08-19 12:49:22 -0400

    todas as variaveis necessarias postas em um objeto 'formClient' para começar a parte do desenvolvimento com funçÕes

--- orderpag/order_pag.js
+++ orderpag/order_pag.js
@@ -24,7 +24,46 @@ document.addEventListener('DOMContentLoaded', function () {
             medic: document.querySelector("#medic-name"),
             glasses: {
                 glassesClientName: document.querySelector("#glasses-client-name"),
-                
+                prescription: {
+                    rightEye: {
+                        esf: document.querySelector("#esferico-od"),
+                        cil: document.querySelector("#cilindrico-od"),
+                        axis: document.querySelector("#axis-od"),
+                        dnp: document.querySelector("#dnp-od"),
+                        height: document.querySelector("#height-od")
+                    },
+                    leftEye: {
+                        esf: document.querySelector("#esferico-oe"),
+                        cil: document.querySelector("#cilindrico-oe"),
+                        axis: document.querySelector("#axis-oe"),
+                        dnp: document.querySelector("#dnp-oe"),
+                        height: document.querySelector("#height-oe")
+                    }
+                },
+                glassesType: document.querySelector("#lens-select"),
+                frames: {
+                    frameId: document.querySelector("#frames"),
+                    price: document.querySelector("#frames-value"),
+                    obs: document.querySelector("#obs-glasses-label")
+                }
+            },
+            payment: {
+                price: document.querySelector("#price"),
+                dicount: document.querySelector("#discount"),
+                finalPrice: document.querySelector("#final-price"),
+                method: document.querySelector("#payment-method"),
+                paid: document.querySelector("#value-check")
+            },
+            signatureAndDocuments: {
+                canvas: document.querySelector("#canvas"),
+                frontDocument: document.querySelector("#front-document"),
+                backDocument: document.querySelector("#back-document"),
+                limparCanvas: function() {
+
+                },
+                salvarAssinatura: function () {
+                    
+                }
             }
 
         }
