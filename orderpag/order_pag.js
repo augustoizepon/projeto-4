aa7c4c5a9f126d7fe93b9368cb6280a3f290191b
augusto.izepon <augustom.izepon@gmail.com>
2023-08-18 22:38:11 -0400

    comecando a adicionar as variaveis para formar o objeto do cliente

--- orderpag/order_pag.js
+++ orderpag/order_pag.js
@@ -2,25 +2,40 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     function uploadContent() {
-        const lerInputs = () => {
-            const input = document.querySelectorAll('input');
-            let array = []
-            input.forEach(valores => {
-                array.push(`{${valores.placeholder}: ${valores.value}}`)
-            })
-            console.log(array)
+
+
+
+    }
+    const lerInputs = () => {
+
+        const formCLient = {
+            name: document.querySelector("#name"),
+            cpf: document.querySelector("#cpf"),
+            birth: document.querySelector("#birth"),
+            phone: document.querySelector("#phone"),
+            address: {
+                street: document.querySelector("#address"),
+                residencialNumber: document.querySelector("#residencial-number"),
+                neighborhood: document.querySelector("#bairro"),
+                city: document.querySelector("#city"),
+                state: document.querySelector("#state"),
+                postalCode: document.querySelector("#CEP")
+            },
+            medic: document.querySelector("#medic-name"),
+            glasses: {
+                glassesClientName: document.querySelector("#glasses-client-name"),
+                
+            }
+
         }
 
 
         lerInputs()
 
-
+        const btnUpload = document.querySelector('#send')
+        btnUpload.addEventListener('click', uploadContent)
     }
-
-
-    const btnUpload = document.querySelector('#send')
-    btnUpload.addEventListener('click', uploadContent)
-})
+});
 
 
 
